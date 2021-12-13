45ce79c8b7f93d8d8d7f2ed7e79515bcd36ec07f
fabian.toepfer@tu-dresden.de <Fabian Tpfer>
2021-12-13 10:36:53 +0100

    adds rapid mode

--- Assets/Main/Scripts/World.cs
+++ Assets/Main/Scripts/World.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class World : MonoBehaviour
 {
+
+    public bool rapidMode = false;
+    
     private ShapeManager m_shapeManager;
     private AudioManager m_audioManager;
     private InteractionManager m_interactionManager;
-
+ 
      void Start()
     {
         Setup();
@@ -18,15 +21,34 @@ public class World : MonoBehaviour
         m_shapeManager = new ShapeManager();
         m_audioManager = new AudioManager();
         m_interactionManager = new InteractionManager();
-       
+
+
         Debug.Log("[World] Setup finished");
+    }
+
+    private void OnValidate()
+    {
+        Setup();
+
+        if (rapidMode)
+        {
+            RunRapidMode();
+        };
+
+    }
+
+
+    void RunRapidMode()
+    {
+        Debug.Log("[World] Rapid Mode finished");
     }
 
     void Update()
     {
-        
-        m_audioManager.Update();
-        m_shapeManager.Update(m_audioManager.GetCurrentAudioEvent());
+        if (!rapidMode) { 
+            m_audioManager.Update();
+            m_shapeManager.Update(m_audioManager.GetCurrentAudioEvent());
+        }
 
     }
 
